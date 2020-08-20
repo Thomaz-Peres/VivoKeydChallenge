faca2a20bfc18c1febae6096e80745848adb3867
Thomaz Peres <thomaz.peres21@hotmail.com>
2020-08-19 22:59:05 -0300

    Initializing project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f;
+
+    private Rigidbody rigidbody;
+
+    public float jumpForce;
+
+    public float gravityModifier;
+
+    [SerializeField]
+    private float jumpLado;
+
+    public bool onGround = true;
+
+    private bool gameOver = false;
+
+    public float limiteVel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
+        StartCoroutine(AjustaVel());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimentacao();
+
+        Right();
+
+        Left();
+
+        Jump();
+    }
+
+    void Movimentacao()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        onGround = true;
+    }
+
+    void Right()
+    {
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * jumpLado);
+        }
+    }
+
+    void Left()
+    {
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * jumpLado * -1);
+        }
+    }
+
+    IEnumerator AjustaVel()
+    {
+        //ajutasdo minha velocidade
+        while (!gameOver)
+        {
+            yield return new WaitForSeconds(2);
+            if (speed < limiteVel)
+            {
+                speed += 1.0f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatform : MonoBehaviour
+{
+
+    public List<GameObject> platforms = new List<GameObject>();
+
+    public int offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < platforms.Count; i++)
+        {
+            // lista de objetos a ser instanciados, a posição que vao ser criados e a rotação
+            Instantiate(platforms[i], new Vector3(0, 0, i * 86), transform.rotation);
+            offset += 86;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Recycle(GameObject platform)
+    {
+        platform.transform.position = new Vector3(0, 0, offset);
+        offset += 86;
+    }
+}
