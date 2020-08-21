5d6245c15baf292b684d4884cfb00b15aad1e51c
Thomaz Peres <thomaz.peres21@hotmail.com>
2020-08-20 22:56:43 -0300

    Modifing codes

--- Assets/Scripts/SpawnPlatform.cs
+++ Assets/Scripts/SpawnPlatform.cs
@@ -5,25 +5,49 @@ using UnityEngine;
 public class SpawnPlatform : MonoBehaviour
 {
 
-    public List<GameObject> platforms = new List<GameObject>();
+    public List<GameObject> platforms = new List<GameObject>(); // prefabs
+    public List<Transform> currentPlatforms = new List<Transform>();    //  objetos instanciados
 
     public int offset;
+
+    private Transform player;
+    private Transform currentPlatformPoint;
+    private int platformIndex;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
         for (int i = 0; i < platforms.Count; i++)
         {
             // lista de objetos a ser instanciados, a posição que vao ser criados e a rotação
-            Instantiate(platforms[i], new Vector3(0, 0, i * 86), transform.rotation);
+            Transform p = Instantiate(platforms[i], new Vector3(0, 0, i * 86), transform.rotation).transform;
+            currentPlatforms.Add(p);
             offset += 86;
         }
+
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<FinalPoint>().point;
+        //  acima o currentPlatformPoint recebe o ponto do final da plataforma
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = player.position.z - currentPlatformPoint.position.z;
+
+        if(distance >= 5)
+        {
+            Recycle(currentPlatforms[platformIndex].gameObject);
+            platformIndex++;
+
+            if(platformIndex > currentPlatforms.Count - 1)
+            {
+                platformIndex = 0;
+            }
+
+            currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<FinalPoint>().point;
+        }
     }
 
     public void Recycle(GameObject platform)
